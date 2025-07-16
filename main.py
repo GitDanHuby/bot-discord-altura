c623bf5bd1553951bc51f0a6ef6e013c28c8a137
GitDanHuby <157174289+GitDanHuby@users.noreply.github.com>
2025-07-16 13:28:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -39,9 +39,9 @@ voice_join_times = {} # <--- ADICIONE ESTA LINHA
 
 # --- LISTA DE STATUS PARA O CARROSSEL ---
 status_list = cycle([
-    discord.Activity(type=discord.ActivityType.watching, name="🚓 patrulhando Altura RP"),
-    discord.Activity(type=discord.ActivityType.watching, name="a cidade crescer!"),
-    discord.Activity(type=discord.ActivityType.listening, name="as melhores da rádio"),
+    discord.Activity(type=discord.ActivityType.custom, state="🚓 Patrulhando Altura RP", state="🚓"),
+    discord.Activity(type=discord.ActivityType.watching, state=f"a cidade crescer!"),
+    discord.Activity(type=discord.ActivityType.listening, state="as melhores da rádio"),
     discord.Game(name="com o comando /ip")
 ])
 
